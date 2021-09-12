a5e1782ea1de569052b8fb54f2fabafcd7365d2c
Laith Harb <35265876+harblaith7@users.noreply.github.com>
2021-09-12 11:18:13 -0400

    Update starting-code.tsx

--- src/starting-code.tsx
+++ src/starting-code.tsx
@@ -21,7 +21,7 @@ function App() {
           <div className="customer-food-card-container">
             <p>Selena Gomez</p>
             <div className="customer-foods-container">
-              <div className="customer-food-container"></div>
+              <div className="customer-food"></div>
               <div className="customer-food-input-container">
                 <input />
                 <button>Add</button>
